7db1350479f276917080764c907d6b9d871cd36a
rhj <1419562890@qq.com>
2024-10-16 19:21:50 +0800

    新增Vue项目中的响应式示例文件
    在Vue项目中添加了两个新的示例文件，分别展示了 reactive 和 ref 两种响应式数据处理方法。修改了 main.ts 文件，将根组件指向新的 ref 示例文件。这两个示例帮助开发者理解Vue3中如何使用这些API来创建响应式数据。

---
+++ day03/vue-project/src/04-响应式reactive.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+// 1. 导入函数
+import { reactive } from "vue";
+// 2. 创建响应式数据对象
+const state = reactive({ name: 'tom', age: 18 })
+</script>
+
+<template>
+    <div>
+        <p>姓名：{{ state.name }}</p>
+        <p>年龄：{{ state.age }} <button @click="state.age++">一年又一年</button></p>
+    </div>
+</template>
+
---
+++ day03/vue-project/src/05-响应式ref.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const count = ref(100)
+const add = () => {
+    count.value++
+}
+const sub = () => {
+    count.value--
+}
+</script>
+
+<template>
+    <button @click="sub()">-</button>
+    <span>{{ count }}</span>
+    <button @click="add()">+</button>
+</template>
+
+<style scoped>
+button {
+    background-color: #007bff;
+    color: white;
+    border: none;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 5px;
+    margin: 5px;
+}
+</style>
