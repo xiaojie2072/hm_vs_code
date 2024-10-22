1c9d1274985a8b098c43c998bba6e25b0b1fb474
rhj <1419562890@qq.com>
2024-10-22 09:24:26 +0800

    111

---
+++ day04-黑马头条/vue_hm_news/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import ChannelNav from './components/ChannelNav.vue';
+import ArticleList from './components/ArticleList.vue'
+</script>
+
+<template>
+  <ChannelNav></ChannelNav>
+  <ArticleList></ArticleList>
+</template>
