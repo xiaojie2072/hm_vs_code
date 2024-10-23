6646398bf15c742d8e3cc4cf967663681e323de7
rhj <1419562890@qq.com>
2024-10-23 16:45:13 +0800

    h5-vant 组件库整合

--- day06/vue-meikou/src/main.ts
+++ day06/vue-meikou/src/main.ts
@@ -1,5 +1,24 @@
+import {
+  Button,
+  Cell,
+  CellGroup,
+  Image,
+  Field,
+  ActionSheet,
+  RadioGroup,
+  Radio,
+  DatePicker,
+  Popup,
+  Picker,
+  Area,
+  Icon,
+  SwipeCell,
+  Checkbox,
+  Popover,
+  PullRefresh,
+} from 'vant'
 import './assets/main.css'
-
+import 'vant/lib/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -10,5 +29,21 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(Button)
+app.use(Cell)
+app.use(CellGroup)
+app.use(Image)
+app.use(Field)
+app.use(ActionSheet)
+app.use(RadioGroup)
+app.use(Radio)
+app.use(DatePicker)
+app.use(Popup)
+app.use(Picker)
+app.use(Area)
+app.use(Icon)
+app.use(SwipeCell)
+app.use(Checkbox)
+app.use(Popover)
+app.use(PullRefresh)
 app.mount('#app')
